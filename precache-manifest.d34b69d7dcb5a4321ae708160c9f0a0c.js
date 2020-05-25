540eb4d90ba604a279115ea0e046b87f94faee95
leo-adm <leoaverdalmass@gmail.com>
2020-05-25 02:00:04 -0300

    Updates

---
+++ precache-manifest.d34b69d7dcb5a4321ae708160c9f0a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e4c8889044f727453365657c331b76",
+    "url": "/QRCodeFlex/index.html"
+  },
+  {
+    "revision": "9dbccdea9db8ab81c608",
+    "url": "/QRCodeFlex/static/css/main.10c871b7.chunk.css"
+  },
+  {
+    "revision": "b160d284f626c57aa069",
+    "url": "/QRCodeFlex/static/js/2.2e15270c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QRCodeFlex/static/js/2.2e15270c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dbccdea9db8ab81c608",
+    "url": "/QRCodeFlex/static/js/main.ae270845.chunk.js"
+  },
+  {
+    "revision": "f8f253b3bf6d44fe55ca",
+    "url": "/QRCodeFlex/static/js/runtime-main.85be21b7.js"
+  }
+]);
